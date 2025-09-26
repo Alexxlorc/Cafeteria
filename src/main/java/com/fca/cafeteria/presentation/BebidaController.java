60e4a8946d5d9a7292fccd658deabb8b08e69a78
Alejandro Diaz Castellanos <alex.diaz@comunidad.unam.mx>
2025-09-25 21:38:20 -0600

    Agregue Controller

--- src/main/java/com/fca/cafeteria/presentation/BebidaController.java
+++ src/main/java/com/fca/cafeteria/presentation/BebidaController.java
@@ -1,8 +1,49 @@
 package com.fca.cafeteria.presentation;
 
-public class BebidaController {
+import com.fca.cafeteria.data.Bebida;
+import com.fca.cafeteria.data.TipoBebida;
+import com.fca.cafeteria.domain.BebidaDomain;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
+@Controller
+@RequestMapping("/cafeteria")
+public class CafeteriaController {
 
+    @Autowired
+    private BebidaDomain bebidaDomain;
 
+    //TipoBebida
+    @RequestMapping(value = "/tipo/agregar", method = RequestMethod.POST)
+    @ResponseBody
+    public List<TipoBebida> agregarTipo(@RequestParam String descripcion) {
+        return bebidaDomain.agregarTipoBebida(descripcion);
+    }
+
+    
+    @RequestMapping(value = "/tipo/todos", method = RequestMethod.GET)
+    @ResponseBody
+    public List<TipoBebida> listarTipos() {
+        return bebidaDomain.listarTipos();
+    }
+
+    //Bebida 
+  
+    @RequestMapping(value = "/bebida/agregar", method = RequestMethod.POST)
+    @ResponseBody
+    public List<Bebida> agregarBebida(@RequestParam String nombre,
+                                      @RequestParam String descripcion,
+                                      @RequestParam Integer idTipoBebida) {
+        return bebidaDomain.agregarBebida(nombre, descripcion, idTipoBebida);
+    }
+
+   
+    @RequestMapping(value = "/bebida/por-tipo", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Bebida> listarPorTipo(@RequestParam Integer idTipoBebida) {
+        return bebidaDomain.listarBebidasPorTipo(idTipoBebida);
+    }
 }
